28c2045e1488404a9fbe2c9cdc1e4edaef245e7e
vatrasar <vatrasar@op.pl>
2018-06-10 13:00:58 +0200

    rozpoczecie projektu, dodanie plikow do rejestracji

---
+++ src/main/java/net/zembrowski/julian/JulianApplication.java
@@ -0,0 +1,12 @@
+package net.zembrowski.julian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JulianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JulianApplication.class, args);
+	}
+}
---
+++ src/main/java/net/zembrowski/julian/Starter.java
@@ -0,0 +1,12 @@
+package net.zembrowski.julian;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Starter implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
