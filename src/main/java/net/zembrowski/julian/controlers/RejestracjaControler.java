54606adcc98f94a65df0dc6e740a6e07977b0d60
vatrasar <vatrasar@op.pl>
2018-06-10 17:17:54 +0200

    add cotroler to hello

--- src/main/java/net/zembrowski/julian/controlers/RejestracjaControler.java
+++ src/main/java/net/zembrowski/julian/controlers/RejestracjaControler.java
@@ -5,6 +5,7 @@ import net.zembrowski.julian.domain.Uzytkownik;
 import net.zembrowski.julian.services.UzytkownikService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,11 +15,28 @@ public class RejestracjaControler {
     @Autowired
     UzytkownikService uzytkownicy;
 
+
+
     @RequestMapping(value ="/rejestruj",method = RequestMethod.POST)
-    public String rejestruj(Uzytkownik nowyUzytkownik)
+    public String dojajUzytkownikarejestruj(Uzytkownik nowyUzytkownik)
     {
         uzytkownicy.addUzytkownik(nowyUzytkownik);
+        return "redirect:/hello";
+    }
+
+    @RequestMapping(value ="/rejestruj")
+    public String rejestruj(Model model)
+    {
+
+        model.addAttribute("urzytek",new Uzytkownik());
         return "rejestracja";
     }
+
+    @RequestMapping(value ="/hello")
+    public String powitanie(Model model)
+    {
+        return "hello";
+    }
+
 
 }
