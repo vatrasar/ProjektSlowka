b7556721c94d319dca14181641f121d5b1906def
vatrasar <vatrasar@op.pl>
2018-06-10 16:22:27 +0200

    add getter and setter to Uzytkownik

--- src/main/java/net/zembrowski/julian/domain/Uzytkownik.java
+++ src/main/java/net/zembrowski/julian/domain/Uzytkownik.java
@@ -6,7 +6,24 @@ import javax.persistence.Id;
 @Entity
 public class Uzytkownik {
 
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getHaslo() {
+        return haslo;
+    }
+
+    public void setHaslo(String haslo) {
+        this.haslo = haslo;
+    }
+
     @Id
+
     String login;
     String haslo;
 
