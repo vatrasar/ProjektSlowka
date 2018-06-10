eaaf8a61bffa4d61d9c7495d68110fa384d6bafb
vatrasar <vatrasar@op.pl>
2018-06-10 17:18:35 +0200

    add SecurityConf class

---
+++ src/main/java/net/zembrowski/julian/config/SecurityConf.java
@@ -0,0 +1,22 @@
+package net.zembrowski.julian.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+public class SecurityConf extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity security) throws Exception {
+        security.authorizeRequests().antMatchers("/rejestruj").permitAll()
+                .antMatchers("/hello").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().defaultSuccessUrl("/rejestracja");
+    }
+}
