aaa324e931107c16e582cceb4ca3d87c1ad16c04
vatrasar <vatrasar@op.pl>
2018-06-11 23:24:27 +0200

    add tomcat dependency

--- src/main/java/net/zembrowski/julian/JulianApplication.java
+++ src/main/java/net/zembrowski/julian/JulianApplication.java
@@ -2,12 +2,19 @@ package net.zembrowski.julian;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JulianApplication {
+public class JulianApplication  extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(JulianApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(JulianApplication.class);
 	}
 }
