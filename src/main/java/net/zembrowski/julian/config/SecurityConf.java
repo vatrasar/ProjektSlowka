b66aa9a0a9b17e7e5851667272e991756f506280
vatrasar <vatrasar@gmail.com>
2019-02-17 11:33:12 +0100

    fix problem with bootstrap

--- src/main/java/net/zembrowski/julian/config/SecurityConf.java
+++ src/main/java/net/zembrowski/julian/config/SecurityConf.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
@@ -48,6 +49,12 @@ public class SecurityConf extends WebSecurityConfigurerAdapter {
                 .authoritiesByUsernameQuery("SELECT username,role FROM Role WHERE username = ?");
 
 
+    }
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**","/webjars/**");
     }
 
 }
