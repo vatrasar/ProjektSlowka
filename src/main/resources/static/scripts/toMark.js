4d9bd785aa646b49452f6e6a7a09b343234e646a
vatrasar <vatrasar@gmail.com>
2019-02-23 22:56:08 +0100

    fix problem with preventDefault in toMark script

--- src/main/resources/static/scripts/toMark.js
+++ src/main/resources/static/scripts/toMark.js
@@ -1,19 +1,20 @@
 $('table').on('click',function (event) {
-    event.preventDefault();
-    if(event.target.className==="toMark")
-    {
+    if(event.target.textContent==="zaznacz" || event.target.textContent==="odznacz")
+        event.preventDefault();
+        if(event.target.className==="toMark")
+        {
 
-        $.get(event.target.getAttribute("href"),function (data) {
-            $row=$(event.target.parentNode.parentNode);
-            $row.children().removeClass();
-            $row.children().attr("class",data);
+            $.get(event.target.getAttribute("href"),function (data) {
+                $row=$(event.target.parentNode.parentNode);
+                $row.children().removeClass();
+                $row.children().attr("class",data);
 
-        });
-        if(event.target.textContent==="zaznacz")
-            event.target.textContent="odznacz";
-        else
-            event.target.textContent="zaznacz";
-    }
+            });
+            if(event.target.textContent==="zaznacz")
+                event.target.textContent="odznacz";
+            else
+                event.target.textContent="zaznacz";
+        }
 
 
 });
