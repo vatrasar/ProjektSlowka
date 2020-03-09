fe961c7289e5bf759d0f166e1b99f206efaa8168
vatrasar <vatrasar@gmail.com>
2020-03-09 20:19:30 +0100

    add latexKonf

--- src/main/java/net/zembrowski/julian/Utils/LatexGenerationUtils.java
+++ src/main/java/net/zembrowski/julian/Utils/LatexGenerationUtils.java
@@ -75,11 +75,135 @@ public class LatexGenerationUtils {
 
                 try {
                     Files.createDirectories(Paths.get("latexProject/zdjecia"));
+                    FileWriter writer=new FileWriter("latexProject/latexKonf.tex");
+                    writer.write(getLatexKonfString());
+                    writer.close();
                 } catch (IOException e1) {
                     e1.printStackTrace();
                 }
             }
         }
+    }
+
+    private static String getLatexKonfString() {
+
+        return "\\documentclass[a4paper,polish,12pt]{article}\n" +
+                "    \\usepackage[utf8]{inputenc}\n" +
+                "    \\usepackage[T1]{fontenc}\n" +
+                "    \\usepackage{lmodern}\n" +
+                "\t\\usepackage{amsmath}\n" +
+                "\t\\usepackage{xcolor}\n" +
+                "    \\usepackage{babel}\n" +
+                "    \\usepackage{csquotes}\n" +
+                "    \\DeclareQuoteAlias{croatian}{polish}\n" +
+                "    \\usepackage[%\n" +
+                "    style=verbose-ibid, % numeric, alphabetic, authoryear, ect.\n" +
+                "    sorting=nty,\n" +
+                "    isbn=false,\n" +
+                "    abbreviate = false,\n" +
+                "    backend=biber,]{biblatex}\n" +
+                "\n" +
+                "    \\renewcommand*{\\newunitpunct}{\\addcomma\\space}\n" +
+                "    \\renewbibmacro*{in:}{}\n" +
+                "\n" +
+                "    \\usepackage{xpatch}\n" +
+                "\n" +
+                "    \\xpatchbibdriver{book}{%\n" +
+                "    \\newunit\n" +
+                "    \\iffieldundef{maintitle}\n" +
+                "    {\\printfield{volume}%\n" +
+                "    \\printfield{part}}\n" +
+                "    {}%\n" +
+                "    }\n" +
+                "    {%\n" +
+                "    }{}{}\n" +
+                "\n" +
+                "    \\xpatchbibdriver{book}{%\n" +
+                "    \\usebibmacro{publisher+location+date}%\n" +
+                "    }\n" +
+                "    {%\n" +
+                "    \\usebibmacro{publisher+location+date}%\n" +
+                "    \\newunit\n" +
+                "    \\printfield{volume}%\n" +
+                "    \\printfield{part}\n" +
+                "    \\usebibmacro{finentry}\n" +
+                "    }{}{}\n" +
+                "\n" +
+                "    \\DeclareFieldFormat{journaltitle}{\\mkbibquote{#1}}\n" +
+                "    \\DeclareFieldFormat[article,periodical]{number}{nr.  #1}% number of a journal\n" +
+                "\n" +
+                "    \\DeclareFieldFormat\n" +
+                "    [article,inbook,incollection,inproceedings,patent,thesis,unpublished]\n" +
+                "    {title}{\\mkbibemph{#1}}\n" +
+                "    %\n" +
+                "    \\renewbibmacro*{journal+issuetitle}{%\n" +
+                "    \\usebibmacro{journal}%\n" +
+                "    \\setunit*{\\addspace}%\n" +
+                "    \\iffieldundef{series}\n" +
+                "    {}\n" +
+                "    {\\newunit\n" +
+                "    \\printfield{series}%\n" +
+                "    \\setunit{\\addspace}}%\n" +
+                "    \\usebibmacro{issue+date}%\n" +
+                "    \\setunit{\\addspace}%\n" +
+                "    \\usebibmacro{issue}%\n" +
+                "    \\setunit{\\addspace}%\n" +
+                "    \\usebibmacro{volume+number+eid}%\n" +
+                "    \\newunit}\n" +
+                "\n" +
+                "    \\renewbibmacro*{issue+date}{%\n" +
+                "    \\iffieldundef{issue}\n" +
+                "    {\\usebibmacro{date}}\n" +
+                "    {\\printfield{issue}%\n" +
+                "    \\setunit*{\\addspace}%\n" +
+                "    \\usebibmacro{date}}%\n" +
+                "    \\newunit}\n" +
+                "\n" +
+                "    \\renewbibmacro*{publisher+location+date}{%\n" +
+                "    \\printlist{publisher}%\n" +
+                "    \\iflistundef{publisher}\n" +
+                "    {\\setunit*{\\addcomma\\space}}\n" +
+                "    {\\setunit*{\\addcomma\\space}}%\n" +
+                "    \\printlist{location}%\n" +
+                "    \\setunit*{\\addspace}%\n" +
+                "    \\usebibmacro{date}%\n" +
+                "    \\newunit}\n" +
+                "\\usepackage{graphicx}\n" +
+                "\\usepackage{color}\n" +
+                "\n" +
+                "\\usepackage{listings}\n" +
+                " \\renewcommand{\\lstlistingname}{kod} %zmiana podpisu na polski\n" +
+                "\n" +
+                "\\definecolor{dkgreen}{rgb}{0,0.6,0}\n" +
+                "\\definecolor{gray}{rgb}{0.5,0.5,0.5}\n" +
+                "\\definecolor{mauve}{rgb}{0.58,0,0.82}\n" +
+                "\n" +
+                "\n" +
+                "\\lstset{frame=tb,\n" +
+                " language=Python,\n" +
+                " aboveskip=3mm,\n" +
+                " belowskip=3mm,\n" +
+                " showstringspaces=false,\n" +
+                " columns=flexible,\n" +
+                " basicstyle={\\small\\ttfamily},\n" +
+                " numbers=none,\n" +
+                " numberstyle=\\tiny\\color{gray},\n" +
+                " keywordstyle=\\color{blue},\n" +
+                " commentstyle=\\color{dkgreen},\n" +
+                " stringstyle=\\color{mauve},\n" +
+                " breaklines=true,\n" +
+                " breakatwhitespace=true,\n" +
+                " tabsize=3\n" +
+                "}\n" +
+                "\n" +
+                "\\usepackage{hyperref}\n" +
+                "\\newtheorem{theorem}{Twierdzenie}\n" +
+                "\\addbibresource{bik.bib}\n" +
+                "\\newtheorem{tw}{Twierdzenie}\n" +
+                "\\title{Projekt Cyfrowe przetwarzanie sygnałów}\n" +
+                "\\date{}\n" +
+                "\\author{Szymon Kozakiewicz}";
+
     }
 
 }
