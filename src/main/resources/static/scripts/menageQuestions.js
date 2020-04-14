c92bb6c71d015d1e62ae6f09df114f4aa3cf56e7
vatrasar <vatrasar@gmail.com>
2020-04-14 15:44:36 +0200

    remove tabels for empty sections

--- src/main/resources/static/scripts/menageQuestions.js
+++ src/main/resources/static/scripts/menageQuestions.js
@@ -13,9 +13,55 @@ $(".sectionSelect").on("change",afterChangeOption);
 
 function afterChangeOption() {
     var selected = $(this).children("option:selected");
-    var row=$(this).parent().parent();
-    $.get("/updateSection?id="+selected.attr("name")+"&selected="+selected.val());
-    $("table[name='"+selected.val()+"']").append(row);
+    if(selected.val()==="Nowa sekcja")
+    {
+        var newSection = prompt("Wprowadź nazwe nowej sekcji:", selected.val());
+
+        var $newTable=$(this).parent().parent().parent().parent().clone();
+
+        $newTable.find(".questionRow").remove();
+        $newTable.find("caption").text(newSection);
+        $newTable.attr("name",newSection);
+
+
+
+        $("#tablesContainer").append($newTable);
+        $.get("/updateSection?id="+selected.attr("name")+"&selected="+newSection);
+
+        addNewOptionToAllRows(newSection);
+
+        var $row=$(this).parent().parent();
+
+        $row.find("option[value='"+newSection+"']").prop('selected', true);
+        $("table[name='"+newSection+"']").append($row);
+
+
+    }
+    else {
+        var row=$(this).parent().parent();
+
+        var $rowParrent=row.parent();
+
+        $.get("/updateSection?id=" + selected.attr("name") + "&selected=" + selected.val());
+        $("table[name='" + selected.val() + "']").append(row);
+        var childrens=$rowParrent.children();
+        if($rowParrent.children().length===1)
+        {
+            $rowParrent.parent().remove();
+        }
+    }
+
+
+}
+
+function addNewOptionToAllRows(newOptionName) {
+   $(".sectionSelect").each(function () {
+      var firstOption= $(this).children().first();
+       var newOption=firstOption.clone();
+       newOption.text(newOptionName);
+       newOption.val(newOptionName);
+       $(this).append(newOption);
+   });
 
 
 }
